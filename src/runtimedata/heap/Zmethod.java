32f9c90c28e5e65b163d8071339090e7170c5e5f
koi2000 <koi20000@163.com>
2023-08-23 00:17:20 +0800

    feat: 暂时提交

--- src/runtimedata/heap/Zmethod.java
+++ src/runtimedata/heap/Zmethod.java
@@ -64,5 +64,122 @@ public class Zmethod extends ClassMember{
     }
 
     // JVM并没有规定如何实现和调用本地方法，这里依然使用JVM栈来执行本地方法
-    //
+    // 但是本地方法中并不包含字节码，那么本地方法的调用，这里我们利用接口来实现调用对应的方法；
+    // 同时 JVM 中预留了两条指令，操作码分别是 0xff 和 0xfe，下面使用 0xfe 来当前方法为表示本地方法
+    // 第二个字节为本地方法的返回指令，该指令和普通方法的返回指令是一样的。
+    private void injectCodeAttribute(String returnType){
+        // 本地方法的操作数栈暂时为4，至少能容纳返回值
+        this.maxStack = 4;
+        // 本地方法的局部变量表只用来存放参数，因此直接赋值
+        this.maxLocals = this.argSlotCount;
+        //接下来为本地方法构造字节码:起始第一个字节都是0xfe,用来表用这是本地方法;
+        //第二个字节码则根据不同的返回值类型选择相应的xreturn的指令即可
+        //不必担心下面的 byte 的强转，因为在读取字节码时，使用的是 readUint8()方法
+        switch (returnType.charAt(0)) {
+            case 'V':
+                this.code = new byte[]{(byte) 0xfe, (byte) 0xb1}; // return
+                break;
+            case 'L':
+            case '[':
+                this.code = new byte[]{(byte) 0xfe, (byte) 0xb0}; // areturn
+                break;
+            case 'D':
+                this.code = new byte[]{(byte) 0xfe, (byte) 0xaf}; // dreturn
+                break;
+            case 'F':
+                this.code = new byte[]{(byte) 0xfe, (byte) 0xae}; // freturn
+                break;
+            case 'J':
+                this.code = new byte[]{(byte) 0xfe, (byte) 0xad}; // lreturn
+                break;
+            default:
+                this.code = new byte[]{(byte) 0xfe, (byte) 0xac};// ireturn
+        }
+    }
+
+    public static Zmethod[] makeMethods(Zclass zclass, MemberInfo[] classFileMethods) {
+        Zmethod[] methods = new Zmethod[classFileMethods.length];
+        for (int i = 0; i < methods.length; i++) {
+            Zmethod method = new Zmethod(zclass, classFileMethods[i]);
+            methods[i] = method;
+        }
+        return methods;
+    }
+
+    public boolean isSynchronized() {
+        return 0 != (accessFlags & AccessFlag.ACC_SYNCHRONIZED);
+    }
+
+    public boolean isBridge() {
+        return 0 != (accessFlags & AccessFlag.ACC_BRIDGE);
+    }
+
+    public boolean isVarargs() {
+        return 0 != (accessFlags & AccessFlag.ACC_VARARGS);
+    }
+
+    public boolean isNative() {
+        return 0 != (accessFlags & AccessFlag.ACC_NATIVE);
+    }
+
+    public boolean isAbstract() {
+        return 0 != (accessFlags & AccessFlag.ACC_ABSTRACT);
+    }
+
+    public boolean isStrict() {
+        return 0 != (accessFlags & AccessFlag.ACC_STRICT);
+    }
+
+    public int getMaxStack() {
+        return maxStack;
+    }
+
+    public int getMaxLocals() {
+        return maxLocals;
+    }
+
+    public byte[] getCode() {
+        return code;
+    }
+
+    public int getArgSlotCount() {
+        return argSlotCount;
+    }
+
+    //搜索自身方法的异常处理表,如果能找到对应的异常处理项,则返回其handlerPC字段(指出负责异常处理的catch块的位置),否则返回-1
+    public int findExceptionHandler(Zclass exClazz, int pc) {
+        ExceptionHandler handler = exceptionTable.findExceptionHandler(exClazz, pc);
+        if (handler != null) {
+            return handler.handlerPc;
+        }
+        return -1;
+    }
+
+    public Zclass[] getExceptionTypes() {
+        if (this.exceptions == null) {
+            return new Zclass[0];
+        }
+
+        int[] exceptionIndexTable = exceptions.getExceptionIndexTable();
+        Zclass[] exClasses = new Zclass[exceptionIndexTable.length];
+        RuntimeConstantPool runtimeConstantPool = clazz.getRuntimeConstantPool();
+
+        for (int i = 0; i < exceptionIndexTable.length; i++) {
+            ClassRef classRef = (ClassRef) runtimeConstantPool.getRuntimeConstant(exceptionIndexTable[i]).getValue();
+            exClasses[i] = classRef.resolvedClass();
+        }
+
+        return exClasses;
+    }
+
+
+    public int getLineNumber(int pc) {
+        if (isNative()) {
+            return -2;
+        }
+        if (lineNumberTable == null) {
+            return -1;
+        }
+        return lineNumberTable.getLineNumber(pc);
+    }
 }
